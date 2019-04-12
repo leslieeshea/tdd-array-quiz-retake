fecb5c276ae6900c64904e2f85e3c69abb98a40f
Leslie Shea <leslieyshea@gmail.com>
2019-04-12 15:23:16 -0700

    updated test

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 const big = require('../lib/index');
 
 describe('Big function', () => {
-  it('has removed any numbers less than 5 and created new phrase', () => {
+  it('has removed any numbers less than 5 and changed phrase', () => {
     const numbers = [1, 3, 5, 7, 2];
     const result = big(numbers);
     expect(result).toEqual(['Wow, 7 is so big!']);
